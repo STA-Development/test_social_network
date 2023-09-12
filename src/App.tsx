9292180c4f605ad3332350970c671c75a2a6d72e
Artavazd Nazaryan <artavazdnazaryan@Artavazds-MacBook-Pro.local>
2023-09-12 15:59:11 +0400

    FEAT: tailwind implementing

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="">
+          <h1 className="text-3xl font-bold underline text-red-600">
+              Simple React Typescript Tailwind Sample
+          </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
